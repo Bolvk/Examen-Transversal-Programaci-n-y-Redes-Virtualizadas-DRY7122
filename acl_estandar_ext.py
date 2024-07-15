86c1239ace1ae11c56b950ed2f628f236243f211
Bolvk <167150713+Bolvk@users.noreply.github.com>
2024-07-15 17:44:56 -0400

    Delete acl_estandar_ext.py

--- acl_estandar_ext.py
+++
@@ -1,7 +0,0 @@
-while True:
-    acl=int(input("Ingrese el tipo de ACL a consultar: "))
-
-    if acl <=100:
-        print("La ACL consultada es de tipo estándar.")
-    else:
-        print("La ACL consultada es de tipo extendida")
